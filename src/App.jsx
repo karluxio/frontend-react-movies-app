b6b66bc5e09a3d890cb2d3d9a9705aa1d5b9f764
karlux <karlux.sistemas@gmail.com>
2023-08-17 13:38:35 -0300

    feat: add fetch to get config data

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,43 @@
 import { useEffect } from "react";
-import { fetchDataFromApi } from "./utils/api";
 import { useDispatch, useSelector } from "react-redux";
-import { getApiConfiguration } from "./store/homeSlice";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Footer, Header } from "./components";
+import { Details, Explore, Home, PageNotFound, SearchResult } from "./pages";
+import { getApiConfiguration } from "./store";
+import { fetchDataFromApi } from "./utils";
 
 export const App = () => {
   const dispatch = useDispatch();
   const { url } = useSelector((state) => state.home);
 
   useEffect(() => {
-    apiTesting();
+    fetchApiConfig();
   }, []);
 
-  const apiTesting = () => {
-    fetchDataFromApi("/movie/popular").then((res) => {
-      dispatch(getApiConfiguration(res));
+  const fetchApiConfig = () => {
+    fetchDataFromApi("/configuration").then((res) => {
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      };
+
+      dispatch(getApiConfiguration(url));
     });
   };
 
-  return <div className="App">App {url?.total_pages}</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 };
