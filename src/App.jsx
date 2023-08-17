834593993cd9e7264b79d4f97094bda05bd2d3bc
karlux <karlux.sistemas@gmail.com>
2023-08-17 02:03:44 -0300

    feat: add utils to fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,14 @@
+import { useEffect } from "react";
+import { fetchDataFromApi } from "./utils/api";
+
 export const App = () => {
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
+  const apiTesting = () => {
+    fetchDataFromApi("/movie/popular").then((res) => console.log(res));
+  };
+
   return <div className="App">App</div>;
 };
