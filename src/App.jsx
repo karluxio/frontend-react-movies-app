ffaa25f4abc4d70ffb3fd6e324e91434b3411822
karlux <karlux.sistemas@gmail.com>
2023-08-17 12:00:14 -0300

    feat: use dispatch and useSelector in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useEffect } from "react";
 import { fetchDataFromApi } from "./utils/api";
+import { useDispatch, useSelector } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
 
 export const App = () => {
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
+
   useEffect(() => {
     apiTesting();
   }, []);
 
   const apiTesting = () => {
-    fetchDataFromApi("/movie/popular").then((res) => console.log(res));
+    fetchDataFromApi("/movie/popular").then((res) => {
+      dispatch(getApiConfiguration(res));
+    });
   };
 
-  return <div className="App">App</div>;
+  return <div className="App">App {url?.total_pages}</div>;
 };
