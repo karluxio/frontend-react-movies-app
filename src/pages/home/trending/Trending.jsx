bf1b82a1ce9b2b307e16c3fd1c6adde2c987052d
karlux <karlux.sistemas@gmail.com>
2023-08-17 21:18:36 -0300

    add carousel component

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,5 +1,6 @@
-import { ContentWrapper } from "../../../components";
-import { SwitchTabs } from "../../../components";
+import { useState } from "react";
+import { ContentWrapper, SwitchTabs } from "../../../components";
+import { Carousel } from "../../../components/carousel/Carousel";
 import { useFetch } from "../../../hooks";
 
 export const Trending = () => {
@@ -17,6 +18,7 @@ export const Trending = () => {
         <span className="carouselTitle">Trending</span>
         <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
       </ContentWrapper>
+      <Carousel data={data?.results} loading={loading} />
     </div>
   );
 };
